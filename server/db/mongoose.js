2bf948a696837314fae3c75975b737298d4304fb
aaljazza <aaljazza@gmail.com>
2018-12-24 19:54:51 +0300

    updated info to push to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; //just in server.js to configure mongoose
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp"
+);
 
 module.exports = { mongoose };
