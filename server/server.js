edc27649c2ab1fffaf33e1597ec0902ef6b0f765
aaljazza <aaljazza@gmail.com>
2018-12-25 20:08:49 +0300

    Updated Get Users Me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
+var { authenticate } = require("./middleware/authenticate");
 
 var app = express();
 const port = process.env.PORT;
@@ -112,11 +113,13 @@ app.post("/users", (req, res) => {
       return user.generateAuthToken();
     })
     .then(token => {
-      console.log("before res");
       res.header("x-auth", token).send(user);
-      console.log(`sent request`);
     })
     .catch(e => res.status(400).send(e));
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 // app.post("/todos", (req, res) => {
